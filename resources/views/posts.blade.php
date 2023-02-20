59ab02ab2d6a3b5eae1bea30e28a3eb396e85e75
Yiğit <yigitsert32@gmail.com>
2023-02-20 16:14:09 +0300

    This is commited just to store local project on internet.
    In this commit, posts pages are created.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/first_post">First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat in pariatur soluta! Atque dolorum ex excepturi fugit harum molestiae quidem saepe? Earum quod reiciendis voluptas! Assumenda eaque esse quisquam ullam!</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/second_post">Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat in pariatur soluta! Atque dolorum ex excepturi fugit harum molestiae quidem saepe? Earum quod reiciendis voluptas! Assumenda eaque esse quisquam ullam!</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/third_post">Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat in pariatur soluta! Atque dolorum ex excepturi fugit harum molestiae quidem saepe? Earum quod reiciendis voluptas! Assumenda eaque esse quisquam ullam!</p>
+    </article>
+</body>
+</html>
