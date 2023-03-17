01f596ef46cc90552c7827ae2f6ac2d21e8fbeec
Yiğit <yigitsert32@gmail.com>
2023-03-17 17:32:11 +0300

    Eloquent Relationship (Post-Category)
    Categories are assigned to each post. New model (Category) and migration for the model are created and used eloquent relationship.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];    //  everything is fillable except...
+//    protected $fillable = ['title', 'excerpt', 'body']; //  id is optional and if it is added, inserted value will be set
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
