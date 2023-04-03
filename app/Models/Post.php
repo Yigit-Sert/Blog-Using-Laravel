b36d774b7e9891f38039ab6564e553e7ac1a7d7a
Yiğit <yigitsert32@gmail.com>
2023-04-03 15:57:46 +0300

    Search (Cleaner Way)
    Operating codes are moved onto PostController and readability increased

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,11 +12,21 @@ class Post extends Model
     protected $guarded = ['id'];    //  everything is fillable except...
 
     protected $with = ['category', 'author'];
-    public function category(){
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) => $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+    }
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){ // user_id
+    public function author()
+    { // user_id
         return $this->belongsTo(User::class, 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -18,26 +19,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-
-    return view('posts', [  //  pass them into the view
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [  //  pass them into the view
