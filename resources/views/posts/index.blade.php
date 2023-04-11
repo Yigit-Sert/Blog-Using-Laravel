da9b085c63be1c121f387573b0d77e2c2e97932c
Yiğit <yigitsert32@gmail.com>
2023-04-11 13:59:32 +0300

    Pagination
    Pagination added to the website.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,8 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
             <x-post-grid :posts="$posts"/>
+
+            {{ $posts->links() }}
         @else
             <p class="text-center">No posts yet. Please check back later.</p>
         @endif
